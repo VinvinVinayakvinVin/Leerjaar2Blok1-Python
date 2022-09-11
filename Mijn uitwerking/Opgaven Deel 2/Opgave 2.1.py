3a3b124ea8a354b6337b872d71341137b481d8b1
Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2022-09-11 18:09:24 +0200

    Opdracht 2.1 volledig gemaakt en nagekeken 👌
    Beetje met VSCode rond spelen 😊

--- Mijn uitwerking/Opgaven Deel 2/Opgave 2.1.py
+++ Mijn uitwerking/Opgaven Deel 2/Opgave 2.1.py
@@ -7,3 +7,5 @@ Created on Sun Sep 11 16:59:47 2022
 
 voornaam = input("Type een voornaam: ")
 print("Hoe gaat het met " + voornaam + "?")
+# print("Hoe gaat het met", voornaam + "?")      # methode 2 uitgeprobeerd van het uitwerking.
+# print(f"Hoe gaat het met {voornaam}?")         # methode 3 uitgeprobeerd van het uitwerking.

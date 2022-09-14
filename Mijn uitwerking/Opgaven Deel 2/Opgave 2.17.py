56b604f0ff81cfb71d752d68b0c1201b7f1ee6b9
Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2022-09-14 16:43:46 +0200

    Opgave 2.17 af en nagekeken ↗↗↗↗↗

---
+++ Mijn uitwerking/Opgaven Deel 2/Opgave 2.17.py
@@ -0,0 +1,30 @@
+# -*-coding:utf-8 -*-
+'''
+@File    :   Opgave 2.17.py
+@Time    :   2022/09/14 16:40:24
+@Author  :   Vinayak Shukla Kalapnat 
+@Version :   1.0
+@Desc    :   None
+'''
+
+'''
+Hieronder staat een programma met comments erbij.
+
+    # Hieronder berekenen we de snelheid van de trein tussen Utrecht en Leiden
+    x = float(input('De treinafstand tussen Utrecht en Leiden is:')) # Afstand tussen beide steden met de trein
+    t = float(input('Hoelang doet de trein hierover?:')) # Reisduur tussen beide steden
+    v = x/t # Sneheid is afstand / tijd
+    print(v) # Antwoord printen
+
+Wat vind je van de comments? Welke vind je goed en welke niet? Hoe zou je die veranderen?
+
+Wat zou je weglaten of eventueel nog extra aan comments toevoegen?
+'''
+
+# Ik zou bij het bovenste comment even zetten in wat voor eenheden die snelheid wordt berekend! bv: km/h of m/s.
+# comment van v mag wel weg en ook van het print statement.
+
+'''
+Uitwerking zegt:
+Al het commentaar behalve de eerste regel is overbodig, de code/vraagstelling legt dit al voldoende uit.
+'''

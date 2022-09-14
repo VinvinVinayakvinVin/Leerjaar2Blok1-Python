e382dd4fdba99116164329410182066ca435c17a
Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2022-09-14 16:21:31 +0200

    Opgave 2.13 gemaakt en nagekeken ✅

---
+++ Mijn uitwerking/Opgaven Deel 2/Opgave 2.13.py
@@ -0,0 +1,40 @@
+# -*-coding:utf-8 -*-
+'''
+@File    :   Opgave 2.13.py
+@Time    :   2022/09/14 16:10:09
+@Author  :   Vinayak Shukla Kalapnat 
+@Version :   1.0
+@Desc    :   None
+'''
+
+'''
+Hieronder zijn zes variabelen aangemaakt, met ieder zo zijn eigen naam.
+
+    # Alle cijfers van het 2e jaars tentamen opgeteld.
+    som_van_alle_programmeer_cijfers_van_het_2e_jaars_tentamen = 455
+
+    # Aantal 2e jaars studenten die het tentamen hebben gemaakt
+    z = 65
+
+    Aantal_jongens = 35
+    2ejaar_meisjes = 30
+    Gem = som_van_alle_programmeer_cijfers_van_het_2e_jaars_tentamen / z
+    Verhouding J/M = Aantal_jongens / 2ejaar_meisjes
+
+a. Welke namen hiervan mogen, maar zijn niet geschikt? (m.a.w. de naam mag wel, maar de naam kan beter of is niet volgens afspraak). Hoe zou je die namen veranderen? Hoe zou jij ze noemen?
+
+b. Welke namen zijn fout?
+'''
+
+# antw.a) ⬇️
+# som_van_alle_programmeer_cijfers_van_het_2e_jaars_tentamen ✅
+# 2ejaar_meisjes ❌
+# Verhouding J/M ❌
+# Gem ✅
+# Aantal_jongens ✅
+# z ⬅️❗ Let op, je kan hierbij liever 'n' gebruiken in plaats van 'z'.
+#       'z' meestal gebruikt bij z ∈ Integer Set, of z is een coördinaat, niet geheel getal.
+
+# antw.b) ⬇️
+# 2ejaar_meisjes ✅
+# Verhouding J/M ✅

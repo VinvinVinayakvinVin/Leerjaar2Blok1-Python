0bb4fab1e2c83ea9b65302153d5b7664b8f2dde2
Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2022-09-14 16:31:58 +0200

    Opgave 2.15 ✅🧑‍💻 af en nagekeken

---
+++ Mijn uitwerking/Opgaven Deel 2/Opgave 2.15.py
@@ -0,0 +1,27 @@
+# -*-coding:utf-8 -*-
+'''
+@File    :   Opgave 2.15.py
+@Time    :   2022/09/14 16:28:42
+@Author  :   Vinayak Shukla Kalapnat 
+@Version :   1.0
+@Desc    :   None
+'''
+
+'''
+Keywords mag je niet gebruiken als variabelenaam.
+Keywords hebben ook een andere kleur in je code (anders dan de standaard zwarte kleur).
+Dus je herkent het als je per ongeluk een keyword gebruikt.
+
+Maak de volgende variabelenamen aan en let op de kleur: continue; global, when.
+Welke van deze drie namen mochten dus wel en welke niet gebruikt worden als variabelennaam?
+'''
+
+# continue = 3
+# global = 4
+# when = 10000000
+
+# print(continue)
+# print(global)
+# print(when)
+
+# Antwoord: variabelnaam 'when' mag je wel gebruiken, andere niet!

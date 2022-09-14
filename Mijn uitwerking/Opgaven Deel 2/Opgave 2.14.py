21277945281317b0c791c1e755ed01a40f2776f1
Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2022-09-14 16:27:35 +0200

    Opgave 2.14 af en nagekeken😎

---
+++ Mijn uitwerking/Opgaven Deel 2/Opgave 2.14.py
@@ -0,0 +1,35 @@
+# -*-coding:utf-8 -*-
+'''
+@File    :   Opgave 2.14.py
+@Time    :   2022/09/14 16:22:33
+@Author  :   Vinayak Shukla Kalapnat 
+@Version :   1.0
+@Desc    :   None
+'''
+
+'''
+Hieronder zijn drie variabelen aangemaakt, met ieder zo zijn eigen naam.
+
+    afstand = float(input('Vul de afstand in van Utrecht naar Den Bosch (km):'))
+    t = float(input('Hoe lang deed je over de tocht (uren)?:'))
+    v = afstand/t
+    print(v)
+
+Wat vind je van de naam voor deze variabelen?
+'''
+
+# variablen namen zijn niet echt consistent. Ik zou liever of alles in één letter natuurkundig symbool zetten óf
+# alle variabel namen als volledig zetten, hiermee bedoel ik dat de symbolen in namen worden weergegeven zoals wat ik hieronder heb getyped:
+# afstand
+# tijd
+# snelheid
+
+'''
+Uitwerking zegt:
+
+De variabelenaam afstand is in het Nederlands, terwijl v kort is voor velocity (Engels voor snelheid).
+De variabelenamen zijn dus tweetalig.
+
+Daarnaast is afstand een naam van meerdere karakters terwijl t en v maar een karakter hebben.
+In meeste natuurkunde boeken zou het gebruikelijker zijn om de afstand x te noemen.
+'''

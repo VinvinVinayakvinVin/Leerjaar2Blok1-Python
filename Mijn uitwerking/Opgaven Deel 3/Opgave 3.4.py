60c47cfc34e225b93e3f561ce912d325441d00d1
Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2022-09-18 18:00:44 +0200

    Opgave 3.4 af en nagekeken (✿◕‿◕✿)

---
+++ Mijn uitwerking/Opgaven Deel 3/Opgave 3.4.py
@@ -0,0 +1,21 @@
+# -*-coding:utf-8 -*-
+'''
+@File    :   Opgave 3.4.py
+@Time    :   2022/09/18 17:55:32
+@Author  :   Vinayak Shukla Kalapnat 
+@Version :   1.0
+@Desc    :   None
+'''
+
+'''
+* Schrijf een programma dat de volgende waarde berekent:  𝜋√e
+
+Hint: alles wat je nodig hebt voor deze opgave is te vinden in de math module.
+Termen zijn wel in het Engels.
+'''
+
+import math
+
+
+uitkomst = math.pi * math.sqrt(math.e)
+print(uitkomst)     # prints π√e

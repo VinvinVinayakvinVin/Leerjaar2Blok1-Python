31a5bce234b8d10aa536716e43df5d9dc2a1bd91
Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2022-09-23 21:20:26 +0200

    Opgave 3.10 af en klaar. ℃

--- Mijn uitwerking/Opgaven Deel 3/Opgave 3.10.py
+++ Mijn uitwerking/Opgaven Deel 3/Opgave 3.10.py
@@ -13,6 +13,24 @@
 Doe opgave 3.9 nogmaals, maar nu met een functie uit de random module.
 '''
 
+'''
+Dit is de opgave van 3.9 ⬇️
+** Schrijf een programma dat een willekeurige geheel getal tussen [500 en 2300) uitprint.
+Gebruik hiervoor de random() functie en je creativiteit. Dus geen andere functie zoals in opgave 3.7b. Andere statements zijn wel toegestaan.
+'''
+
 from random import *
 
-print(triangular(0.0, 1.0, 0.5))
+# print(500 + randint(0, 1799))   # randint function [0, 1799] (1800 is exclusief). => 500 + 0 = 500 minimum, 500 + 1799 = 2299 als maximum.
+for i in range(1000):           # Deze laten loopen, om te testen of er 2300 wel in de lijstje staat.
+    print(randint(500, 2300))   # <- Deze heeft als maximum 2300 inclusief.
+    
+
+'''
+Uitwerking:
+
+# import random
+
+#randrange() genereert een random getal in de gegeven range
+print(random.randrange(500, 2300))
+'''

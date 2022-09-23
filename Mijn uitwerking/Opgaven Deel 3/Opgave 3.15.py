0ac1c2a1d1ac7785ead70b60e8407cdb448f7193
Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2022-09-23 22:19:17 +0200

    Opgave 3.15 w.i.p 👷👷👷👓🥸🔎

---
+++ Mijn uitwerking/Opgaven Deel 3/Opgave 3.15.py
@@ -0,0 +1,25 @@
+# -*-coding:utf-8 -*-
+'''
+@File    :   Opgave 3.15.py
+@Time    :   2022/09/23 22:16:41
+@Author  :   Vinayak Shukla Kalapnat 
+@Version :   1.0
+@Desc    :   None
+'''
+
+'''
+** Maak het volgende programma:
+
+Invoer: een decimaal getal r.
+
+Uitvoer: de oppervlakte van een cirkel, met straal r en een aantal regels met
+
+            “Hiep hiep hoera!”.  
+Werking: de oppervlakte van een cirkel bepalen met straal r en uitprinten. Vervolgens print je x-keer de regel
+
+            “Hiep hiep hoera!” 
+onder elkaar uit. Hierbij is x gelijk aan het cijfer voor de komma (punt) van de oppervlakte van de cirkel.
+
+Vereisten: Voor het bepalen van de oppervlakte én het uitprinten “Hiep hiep hoera!” schrijf je twee aparte functies.
+'''
+

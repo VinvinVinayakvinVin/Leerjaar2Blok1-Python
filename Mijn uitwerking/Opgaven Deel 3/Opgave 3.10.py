5185cd48f9e7bcec92bf632771349ab4e4de0c4d
Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2022-09-23 19:15:01 +0200

    Opgave 3.10 w.i.p 👷

---
+++ Mijn uitwerking/Opgaven Deel 3/Opgave 3.10.py
@@ -0,0 +1,18 @@
+# -*-coding:utf-8 -*-
+'''
+@File    :   Opgave 3.10.py
+@Time    :   2022/09/23 18:27:53
+@Author  :   Vinayak Shukla Kalapnat 
+@Version :   1.0
+@Desc    :   None
+'''
+
+'''
+* Onderzoek de random module nog eens (op internet).
+
+Doe opgave 3.9 nogmaals, maar nu met een functie uit de random module.
+'''
+
+from random import *
+
+print(triangular(0.0, 1.0, 0.5))

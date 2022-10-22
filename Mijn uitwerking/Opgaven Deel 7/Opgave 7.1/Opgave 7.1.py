b1fb96b95bcf9165ebe68be3466e47025baaafa4
Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2022-10-22 12:10:08 +0200

    Opgave 7.1 w.i.p 📁📁📁📁📁📁📁

---
+++ Mijn uitwerking/Opgaven Deel 7/Opgave 7.1/Opgave 7.1.py
@@ -0,0 +1,25 @@
+# -*-coding:utf-8 -*-
+'''
+@File    :   Opgave 7.1.py
+@Time    :   2022/10/22 11:47:53
+@Author  :   Vinayak Shukla Kalapnat 
+@Version :   1.0
+@Desc    :   None
+'''
+
+import os
+
+# os.chdir('Tolkien.txt')
+# print("\n\n\n", os.getcwd())
+
+# os.chdir(str(os.getcwd) + "\Opgaven Deel 7\Opgave 7.1\\")
+
+test = str(os.getcwd)
+
+# fin = open(os.getcwd() + "\Opgaven Deel 7\Opgave 7.1\Tolkien.txt")
+# fin = open(path + "\\Tolkien.txt")
+# for line in fin:
+#     word = line.strip()
+#     print(word)
+
+

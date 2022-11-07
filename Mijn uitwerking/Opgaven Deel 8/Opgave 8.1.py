10b2a683e7d79adbeb3e1f2d63380eb21495d53e
Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2022-11-07 13:00:16 +0100

    Opgave 8.1 af en nagekeken 🎱🎱🎱🎱🎱🎱

---
+++ Mijn uitwerking/Opgaven Deel 8/Opgave 8.1.py
@@ -0,0 +1,56 @@
+# -*-coding:utf-8 -*-
+'''
+@File    :   Opgave 8.1.py
+@Time    :   2022/11/07 12:54:52
+@Author  :   Vinayak Shukla Kalapnat 
+@Version :   1.0
+@Desc    :   None
+'''
+
+'''
+1.¶
+* Schrijf een programma dat Hello, World! tienmaal onder elkaar uitprint (nu met een while-loop).
+'''
+
+i = 0
+while i < 10:
+    i = i + 1
+    print("hi", i)
+
+print()
+
+j = 1
+while j <= 10:
+    j += 1
+    print("hi", j)
+
+print()
+
+i = 1
+while i <= 10:
+  print('Hello, World!', i)
+  i += 1
+
+
+
+# Uitwerking:
+'''
+i = 1
+while i <= 10:
+  print('Hello, World!')
+  i += 1
+
+
+
+# Output:
+Hello, World!
+Hello, World!
+Hello, World!
+Hello, World!
+Hello, World!
+Hello, World!
+Hello, World!
+Hello, World!
+Hello, World!
+Hello, World!
+'''

f7b010d2d29eceb52fee68e5673823192a3c8479
Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2022-11-13 13:15:48 +0100

    Opgave 9.3.py
    mijn code is hetzelfde als de uitwerking.

---
+++ Mijn uitwerking/Opgaven Deel 9/Opgave 9.3/Opgave 9.3.py
@@ -0,0 +1,19 @@
+# -*-coding:utf-8 -*-
+'''
+@File    :   Opgave 9.3.py
+@Time    :   2022/11/13 13:13:32
+@Author  :   Vinayak Shukla Kalapnat 
+@Version :   1.0
+@Desc    :   None
+'''
+
+from math import e, pow, sqrt, sin, pi
+
+print(e)
+
+a = 3
+b = 5
+
+x = sqrt(pow(b, a) * sin(a/b))*pi
+
+print(x)

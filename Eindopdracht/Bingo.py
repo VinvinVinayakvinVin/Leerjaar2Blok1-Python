a45aa678525342fd04d7744974f8da0c40fda518
Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2022-11-13 20:38:43 +0100

    Eindopdracht w.i.p (3d array van bingokaarten gemaakt)

--- Eindopdracht/Bingo.py
+++ Eindopdracht/Bingo.py
@@ -15,20 +15,41 @@ file = open("test_input.txt")
 
 file.readline() # skip/overslaan eerste regel van die trekkingsgetallen.
 
-bingokaart = []
+bingokaarten = []
+bingokaart1 = []
+bingokaart2 = []
+bingokaart3 = []
 temp_lijst = []
 
-for line in file.readlines():
-    element = [line.strip()][0].split()
-    # if line.split() != []:
-    #     temp_lijst.append(int(element))
-    if element != []:               # element is hierbij een lijst!?!?!!? vb -> element = ['22', '13', '17', '11', '0']
-        print(element)
-        for i in element:
-            temp_lijst.append(int(i))
-        bingokaart.append(temp_lijst)
+teller = 0
+aantal_bingos_maken = 0
+
+for line in file.readlines():               # line is als vb: line = ['zin_1', 'zin_2', ..., 'zin_n']
+    # Het onderstaande code over rij = ..., zet een string type naar een lijst type.
+    # Het rij ziet er bv zouit: ['2', '0', '12', '3', '7']
+    rij = [line.strip()][0].split()
+    if rij != []:
+        for i in rij:
+            temp_lijst.append(int(i))   # een tijdelijke lijstje, die voegt een getal erin van een rij.
+
+        if teller % 5 == 0:
+            bingokaarten.append([])
+            aantal_bingos_maken += 1
+
+        bingokaarten[aantal_bingos_maken - 1].append(temp_lijst)
+
+        teller += 1
         temp_lijst = []
 
-print(bingokaart)
+print(bingokaarten)
+
+file.close()
+
+file = open("test_input.txt")
+
+trekkingsgetallen = file.readline().replace(",", " ").split()
+for i in range(len(trekkingsgetallen)):
+    trekkingsgetallen[i] = int(trekkingsgetallen[i])
+print(trekkingsgetallen)
 
 file.close()
